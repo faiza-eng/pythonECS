f3cd57226342be3de6c1844672421a4189c93ce5
faiza-eng <shaikhfaiza78@eng.rizvi.edu.in>
2026-04-18 23:05:35 +0530

    Add files via upload

---
+++ Exp4.py
@@ -0,0 +1,7 @@
+principal = float(input("Enter the Principal Amount: "))
+rate = float(input("Enter the Rate of Interest: "))
+time = float(input("Enter the Time (in years): "))
+
+simple_interest = (principal * rate * time) / 100
+
+print("Simple Interest is:", simple_interest)
